ab77e6d8ece6c9bbf62b35bf1a585acdb9395d25
Soham Das <sohamdas.nest@gmail.com>
2025-11-22 03:37:58 +0530

    test: verify pre-commit hooks with fixed eslint

---
+++ test-precommit.ts
@@ -0,0 +1,8 @@
+// Test file with intentional formatting issues
+const testObject = { foo: 'bar', baz: 'qux' };
+
+function testFunction() {
+  return testObject.foo;
+}
+
+export default testFunction;
